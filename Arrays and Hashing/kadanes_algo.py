c4842d1f269bc5f110d7bb8838d3bcf55515aefe
virinchi <rangerdelli@gmail.com>
2023-09-07 00:13:56 +0530

    added kadanes algo

---
+++ Arrays and Hashing/kadanes_algo.py
@@ -0,0 +1,42 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub_arr_sum = nums[0]
+        max_till_now = nums[0]
+        for i in range(1,len(nums)):
+            max_till_now = max(nums[i],max_till_now+nums[i])
+            max_sub_arr_sum = max(max_sub_arr_sum,max_till_now)
+        return max_sub_arr_sum
+        
+print(Solution.maxSubArray(nums = [-2,1,-3,4,-1,2,1,-5,4]))
+
+'''
+TC - O(n)
+SC - O(1)
+
+Approach :
+
+1. Initialization:
+   - Initialize two variables: `max_sub_arr_sum` and `max_till_now`, both set to the value of the first element in the `nums` list (`nums[0]`).
+   - `max_sub_arr_sum` will store the maximum sum of a subarray encountered so far.
+   - `max_till_now` will store the maximum sum ending at the current position (i.e., it keeps track of the current subarray being considered).
+
+2. Iterate Through the List:
+   - Start a loop from the second element of the `nums` list (index 1) and go up to the end of the list.
+   - For each element at position `i`, calculate `max_till_now` as the maximum of two values:
+     - The current element `nums[i]`.
+     - The sum of the current element and the maximum sum ending at the previous position (`max_till_now + nums[i]`).
+   - This step efficiently keeps track of the maximum sum ending at the current position.
+
+3. Update `max_sub_arr_sum`:
+   - In each iteration, update `max_sub_arr_sum` to be the maximum of two values:
+     - The current `max_sub_arr_sum`.
+     - The current `max_till_now`.
+   - This step ensures that `max_sub_arr_sum` always holds the maximum sum of any subarray encountered during the loop.
+
+4. Return Result:
+   - After the loop completes, `max_sub_arr_sum` will contain the maximum sum of a contiguous subarray within the `nums` list.
+   - Return `max_sub_arr_sum` as the final result.
+
+This approach efficiently finds the maximum subarray sum by keeping track of the maximum sum ending at each position and updating the overall maximum accordingly. The time complexity of this algorithm is O(n), where n is the length of the input list `nums`.
+'''
--- Arrays and Hashing/next_permutation.py
+++ Arrays and Hashing/next_permutation.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         req_ind = -1
@@ -13,8 +14,6 @@ class Solution:
         for i in range(len(nums)-1,req_ind,-1):
             if nums[i]>nums[req_ind]:
                 nums[i], nums[req_ind] = nums[req_ind], nums[i]
-                # print(nums[len(nums)-1:req_ind:-1])
-                # nums = nums[:req_ind+1]+nums[len(nums)-1:req_ind:-1]
                 break
         self.reverse(nums, req_ind+1, len(nums)-1)
 
@@ -25,5 +24,30 @@ class Solution:
             nums[start], nums[end] = nums[end], nums[start]
             start+=1
             end-=1
-
         return nums
+    
+print(Solution().nextPermutation(nums = [1,2,3]))
+
+'''
+TC - O(n)
+SC - O(1)
+
+Approach:
+1. Find the first decreasing element:
+   - Initialize `req_ind` to -1.
+   - Iterate through the `nums` list from right to left.
+   - Compare each element `nums[i]` with its left neighbor `nums[i-1]`.
+   - If `nums[i]` is greater than `nums[i-1]`, set `req_ind` to `i-1` and break out of the loop.
+
+2. Check for no next permutation:
+   - If `req_ind` remains -1 after the loop, it means there's no next permutation possible (the list is in descending order).
+   - In this case, reverse the entire `nums` list.
+
+3. Create the next permutation:
+   - If `req_ind` is not -1 (indicating a decreasing element was found), proceed to create the next permutation.
+   - Find the rightmost element in the list that is greater than `nums[req_ind]`.
+   - Swap this rightmost element with `nums[req_ind]` to make the permutation lexicographically greater.
+   - Reverse the portion of the list from `req_ind + 1` to the end to arrange the remaining elements in ascending order.
+
+4. Return the modified `nums` list as the next permutation.
+'''
